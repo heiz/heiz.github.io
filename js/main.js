73d08564f8fdf2f8c88a5bdda9afe64709899307
vic <vickywongkh@gmail.com>
2020-01-15 00:04:27 +0900

    week4
    week4

---
+++ js/main.js
@@ -0,0 +1,65 @@
+      var datarow=[];
+
+      var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1y59MYoTH_Nq5nDgN_Lia56f7HaKCKAXHdV_nnsFHMWg/edit#gid=1599197251?usp=sharing';
+
+      function init() {
+        Tabletop.init( { key: publicSpreadsheetUrl,
+                         callback: showInfo,
+                         simpleSheet: true } )
+
+      }
+
+
+      function showInfo(data, tabletop) {
+        //alert('Successfully processed!')
+        //console.log(data);
+
+       
+        
+        data.forEach(function(data){
+          //console.log(data.TopTag);
+          //console.log(data.TopQuestionCount);
+          datarow.push([data.TopTag, parseInt(data.TopQuestionCount)]);
+          
+        });
+
+      }
+
+
+      window.addEventListener('DOMContentLoaded', init)     
+
+
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+      
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+
+      function drawChart() {
+         var x = [["javascript", 1928377], ["java", 1624004], ["c#", 1372312], ["php", 1325747], ["python", 1322595], ["android", 1243649],["jquery", 973813],["html", 960130],["c++", 649954],["css", 642712]];
+      
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Language');
+        data.addColumn('number', 'Questions Count');
+        data.addRows(x);
+        console.log(x);
+        console.log(datarow);
+
+        // Set chart options
+        var options = {'title':'Top 10 Popular Questions asked on StackOverFlow',
+                       'width':800,
+                       'height':700,
+                       'bar': {groupWidth: '95%'},
+                      'vAxis': { gridlines: { count: 4 } }
+                     };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+
